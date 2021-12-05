52aded1f345e3ea17eb27cf462e57beeb04cb1a6
Денис Карпенко <karpenko.denis75@gmail.com>
2021-12-05 12:11:28 +0300

    add task 18

---
+++ Task18/Program.cs
@@ -0,0 +1,37 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+
+Console.WriteLine($"1 \n 2");
+/*
+
+
+
+bool[] all = {true, false};
+
+bool result = true;
+
+for(int i = 0; i <all.Length; i++)
+{
+    for(int j = 0; j <all.Length; j++)
+    {
+        if( !(all[i] || all[j]) == (!all[i] && all[j]))
+        {
+            Console.WriteLine($"Для набора x = {all[i]}, y = {all[j]} выражение истинно");
+        }
+        else
+        {
+            Console.WriteLine($"Для набора x = {all[i]}, y = {all[j]} выражение ложно");
+            result = false;
+            break;
+        }
+
+    }
+}
+
+if(result)
+{
+    Console.WriteLine($"Доказано что утверждение истинно");
+} else
+{
+    Console.WriteLine($"Доказано что утверждение ложно");
+}*/
